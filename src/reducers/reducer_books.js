fec5873f00e60f0dd8fe61d456423037ba161327
Thomas Latortue <tsl13b@gmail.com>
2018-04-09 17:41:45 -0400

    add book detail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 
 export default () => {
     return [
-        { title : 'Javascript: The Good Parts' },
-        { title : 'Harry Potter' },
-        { title : 'The Dark Tower' },
-        { title : 'Eloquent Ruby' },
+        { title : 'Javascript: The Good Parts', pages: '114' },
+        { title : 'Harry Potter', pages: '791' },
+        { title : 'The Dark Tower', pages : '512' },
+        { title : 'Eloquent Ruby', pages: '296' },
     ];
 }
