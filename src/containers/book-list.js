8bf59aa5d7291fcedf1e6d124a0faa3f327d1867
Thomas Latortue <tsl13b@gmail.com>
2018-04-09 14:07:48 -0400

    allow the selection of books from books reducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
             );
         });
     }
@@ -27,5 +34,13 @@ function mapStateToProps(state) {
     };
 }
 
-// take mapStateToProps; return a container
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+    // whenever selectbook is called, the result should be passed to all reducers
+    return bindActionCreators({ selectBook : selectBook}, dispatch);
+}
+
+// Promote BoOkList from a component to a container - it needs to know about this new
+// dispatch method, selectBook. Make it available as a prop
+// (take mapStateToProps; return a container)
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
